cfaa06c54f482f42811e4dad8ebdc17d88b32d22
ChenYF <594334637@qq.com>
2019-10-15 17:03:37 +0800

    测试用例

--- src/com/cyf/niukepg/跳台阶.java
+++ src/com/cyf/niukepg/跳台阶.java
@@ -7,12 +7,16 @@ package com.cyf.niukepg;
  * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
  */
 public class 跳台阶 {
-    public int JumpFloor(int target) {
+    public static int JumpFloor(int target) {
 
         if (target <= 2) {
             return target;
         }
 
         return JumpFloor(target - 1) + JumpFloor((target - 2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JumpFloor(10));
     }
 }
