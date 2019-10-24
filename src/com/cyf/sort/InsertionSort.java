b6aead14a38afe0723fa3b4f030910a6cadcf13d
ChenYF <594334637@qq.com>
2019-10-24 23:05:30 +0800

    练习

--- src/com/cyf/sort/InsertionSort.java
+++ src/com/cyf/sort/InsertionSort.java
@@ -64,6 +64,29 @@ public class InsertionSort {
         }
     }
 
+    public static void insertionSort4(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+
+            }
+        }
+    }
+
+    public static void insertionSort5(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0 && arr[j] > arr[j + 1]; j--) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+
 
     //交换i、j元素位置
     public static void swap(int[] arr, int i, int j) {
