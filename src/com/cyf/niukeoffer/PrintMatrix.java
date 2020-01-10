580bcc065f0c63ddd930593f4337f584958db500
Coder <594334637@qq.com>
2020-01-10 13:26:49 +0800

    刷题

--- src/com/cyf/niukeoffer/PrintMatrix.java
+++ src/com/cyf/niukeoffer/PrintMatrix.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
  * @Date: 2019/9/30 11:43
  * @Description:输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字， 例如，如果输入如下4 X 4矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
  * 则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
+ * 思考：1.10
  */
-public class 顺时针打印矩阵 {
+public class PrintMatrix {
 
     /**
-     *  从外到里打印
-     *  边界
+     * 从外到里打印
+     * 边界
+     *
      * @param matrix
      * @return
      */
@@ -23,14 +25,15 @@ public class 顺时针打印矩阵 {
 
         int r = 0, c = 0;  //指针
         ArrayList<Integer> result = new ArrayList<>();  //返回值
-        while ((r <= rowIndex) && (c <= colIndex)) {
+        while ((r <= rowIndex) && (c <= colIndex)) {    //确定要绕着转几圈
             getResult(r++, c++, rowIndex--, colIndex--, result, matrix);
         }
         return result;
     }
 
     /**
-     * 打印过程
+     * 打印过程（转圈过程）
+     *
      * @param r
      * @param c
      * @param rowIndex
