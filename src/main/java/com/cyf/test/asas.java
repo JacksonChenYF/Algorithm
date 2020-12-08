5563890ece46b5008d8ebaa9e872a4f29671ef71
yufchen <yufchen@coremail.cn>
2020-12-08 19:51:07 +0800

    测试 #第一次提交

--- src/main/java/com/cyf/test/asas.java
+++ src/main/java/com/cyf/test/asas.java
@@ -1,4 +1,10 @@
 package com.cyf.test;
 
 public class asas {
+    public static void main(String[] args) {
+        System.out.println("第一次提交！");
+        System.out.println("第二次提交！");
+        System.out.println("第三次提交！");
+        System.out.println("第四次提交！");
+    }
 }
