a88d99edfb64e3a2b5eb2257e2fb18a1f50af53e
yufchen <yufchen@coremail.cn>
2020-12-10 16:56:41 +0800

    测试 #提交2

--- src/main/java/com/cyf/test/asas.java
+++ src/main/java/com/cyf/test/asas.java
@@ -2,7 +2,8 @@ package com.cyf.test;
 
 public class asas {
     public static void main(String[] args) {
-        System.out.println("提交1！");
+        System.out.println("提交1！2" );
+        System.out.println("提交2！");
 
     }
 }
