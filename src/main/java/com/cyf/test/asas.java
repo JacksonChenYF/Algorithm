eeccc144373ff5af784d98353e9c36be80efcdd6
yufchen <yufchen@coremail.cn>
2020-12-10 17:46:10 +0800

    粗无 locat
    正确的
    
    zuihou
    
    zuihou
    
    可以了吧

--- src/main/java/com/cyf/test/asas.java
+++ src/main/java/com/cyf/test/asas.java
@@ -3,8 +3,10 @@ package com.cyf.test;
 public class asas {
     public static void main(String[] args) {
         System.out.println("提交1！2" );
-        System.out.println("提交2！");
-        System.out.println("提交   3！");
-        System.out.println("提交4！");
+        System.out.println(n牛逼 );
+
+
+
+        System.out.println("yasd！");
     }
 }
